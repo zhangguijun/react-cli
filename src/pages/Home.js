4045f444e93e2211b729819c04cb27c2f63bb61a
zhangguijun <1725779714@qq.com>
2019-12-23 16:36:10 +0800

    【feature】新增页面加载时 股价屏内容

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,27 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import '../assets/home.less'
 
 export default class Count extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            text: ''
+        }
     }
-    
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                text: 'This is Home Page!!!'
+            })
+        }, 2000)
+    }
+
     render() {
+        let { text } = this.state
         return (
             <div className="home">
-              <h1 className="h-txt">This is Home Page!!!</h1>
+                <h2>homepage</h2>
+                <h1 className="h-txt">{text}</h1>
             </div>
         )
     }
